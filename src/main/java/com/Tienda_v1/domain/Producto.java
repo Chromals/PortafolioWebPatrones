92a72a4e5e95408fabff89de9a015a2b55565d07
DAVID <DAVID@DAVID>
2023-10-19 22:17:19 -0600

    Semana 6

---
+++ src/main/java/com/Tienda_v1/domain/Producto.java
@@ -0,0 +1,38 @@
+package com.Tienda_v1.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="producto")
+
+//Serialización funciona par aalmacenar ciertos datos en la RAM
+public class Producto implements Serializable{
+   
+    private static final long serialVersionUID = 1L; //Para poder hacer el ciclo de la sumatoria del IdCategoria
+    
+    @Id // Es la PK de la tabla
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Los valores generados que tipo de estrategia utilizan BD == Clase
+    @Column(name = "id_producto")//Idemtifica la columna para encontrar la tabla
+    private long idProducto;
+    private String descripcion;
+    private String rutaImagen;
+    private boolean activo;
+
+    public Producto() {
+    }
+
+    public Producto(String descripcion, boolean activo) {
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+    
+    
+}
--- src/main/java/com/Tienda_v1/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/Tienda_v1/service/impl/CategoriaServiceImpl.java
@@ -1,4 +1,4 @@
-package com.tienda_v1.service.impl;
+package com.Tienda_v1.service.impl;
 
 import com.Tienda_v1.dao.CategoriaDao;
 import com.Tienda_v1.domain.Categoria;
@@ -6,22 +6,39 @@ import com.Tienda_v1.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class CategoriaServiceImpl implements CategoriaService{
+public class CategoriaServiceImpl implements CategoriaService {
 
-    @Autowired //Se va a unificar un unico objeto mientra se ejecuta la app
+    @Autowired
     private CategoriaDao categoriaDao;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Categoria> getCategorias(boolean activos) {
-         var lista = categoriaDao.findAll();
-         
-         if(activos){
-             lista.removeIf(e -> !e.isActivo());
-         }
-         
-         return lista;
+        var lista = categoriaDao.findAll();
+        if (activos) {
+            lista.removeIf(e -> !e.isActivo());
+        }
+        return lista;
     }
-    
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
+    }
 }

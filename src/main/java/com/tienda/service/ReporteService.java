d2b38f2d5aef7c91f57decdb0afe583bca9239cc
DAVID <DAVID@DAVID>
2023-12-01 01:10:08 -0600

    Practica 4? completa

---
+++ src/main/java/com/tienda/service/ReporteService.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tienda.service;
+
+import java.util.Map;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+
+/**
+ *
+ * @author DAVID
+ */
+public interface ReporteService {
+    public ResponseEntity<Resource> generaReporte(String reporte, Map<String, Object> parametros, String tipo);
+}
